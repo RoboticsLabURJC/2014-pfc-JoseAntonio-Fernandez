399356d03280c38e13452aa92664755a293889d4
josete <https://github.com/RoboticsURJC-students/2014-pfc-JoseAntonio-Fernandez.git>
2016-12-08 21:03:00 +0100

    result of execution of __init__ trying to recover the map of a zone using a IGN WMS

--- MapClient/__init__.py
+++ MapClient/__init__.py
@@ -5,11 +5,14 @@ wms = WebMapService('http://www.ign.es/wms-inspire/pnoa-ma', version='1.3.0',tim
 img = wms.getmap(layers=['OI.MosaicElement'],
                  styles=['default'],
                  srs='EPSG:25829',
-                 bbox=(-34.0, 22.0, 67.0, 52.0),
+                 bbox=(-2.0, 2.0, 6.0, 5.0),
                  size=(300, 250),
                  format='image/jpeg',
                  transparent=True)
 
-out = open('jpl_mosaic_visb.jpg', 'wb')
-out.write(img.read())
+print(img.read())
+out = open('jpl_mosaic_visb1.jpg', 'xb')
+y = bytes()
+y = img.read()
+out.write(y)
 out.close()
