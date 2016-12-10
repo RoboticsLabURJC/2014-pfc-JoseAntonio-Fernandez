e4c915bd36741ed1c161da9d2f31dabeeb5e7698
josete <https://github.com/RoboticsURJC-students/2014-pfc-JoseAntonio-Fernandez.git>
2016-12-10 19:12:23 +0100

    getBoundingBox v0 added

--- MapClient/__init__.py
+++ MapClient/__init__.py
@@ -1,26 +1,49 @@
 
 from owslib.wms import WebMapService
 
-wms = WebMapService('http://www.ign.es/wms-inspire/pnoa-ma', version='1.3.0')
 
+
+def getBoundingBox(lat, lon, distance):
+    '''
+    version 0.1
+    Function who retrieves the min and max point of a square where the central point is informed
+    :param lat: latitude on decimal
+    :param lon: longitude on decimal
+    :param distance: distance on meters from point to the vertex NOT Used yet
+    :return: bounding points in decimal [0.0,0.0,0.0,0.0]
+    '''
+    normlat = (distance * 0.0000094413)
+    normlon = (distance * 0.0000061250)
+    return (round(lat-normlat,7), round(lon-normlon,7), round(lat+normlat,7), round(lon+normlon,7))
+
+
+wms = WebMapService('http://www.ign.es/wms-inspire/pnoa-ma', version='1.3.0')
+bbox = getBoundingBox(-3.708283, 40.4153774, 500) # plaza mayor
 img = wms.getmap(layers=['OI.OrthoimageCoverage'],
                  styles=['default'],
                  srs='EPSG:4326',
-                 bbox=(-3.6934474484697,40.416876952149,-3.6840060727373,40.423002044655),#Puerta de Alcalá
-                 size=(300, 250),
+                 bbox=bbox,  # Puerta de Alcalá
+                 size=(500, 500),
                  format='image/png',
                  transparent=True)
 
 print(img.read())
-out = open('exampleThatWorks.png', 'xb')
+out = open('plazaMayor500x500.png', 'xb')
 y = bytes()
 y = img.read()
 out.write(y)
 out.close()
 
-#TODO a partir de una latitud y longitud obtener el encuadre para que quede en medio
+
+
+
+
+
+
+#TODO a partir de una latitud y longitud obtener el encuadre para que quede en medio v0 hecho
 #TODO pintar una señal en el medio
 #TODO ver como muestro el recorrido de la aeronave
 #TODO Interfaz grafico
 #TODO Ver actitud, mapa y posteriormente imagen
 #TODO Marcar en un mapa waypoints para generar un path a la aeronave
+#TODO ¿prescindo de owslib?
