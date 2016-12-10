36fa152bc083bb37f1cee625f5cceaac554e2b99
josete <https://github.com/RoboticsURJC-students/2014-pfc-JoseAntonio-Fernandez.git>
2016-12-10 12:07:51 +0100

    First time work

--- MapClient/__init__.py
+++ MapClient/__init__.py
@@ -1,18 +1,26 @@
+
 from owslib.wms import WebMapService
 
-wms = WebMapService('http://www.ign.es/wms-inspire/pnoa-ma', version='1.3.0',timeout=100)
+wms = WebMapService('http://www.ign.es/wms-inspire/pnoa-ma', version='1.3.0')
 
-img = wms.getmap(layers=['OI.MosaicElement'],
+img = wms.getmap(layers=['OI.OrthoimageCoverage'],
                  styles=['default'],
-                 srs='EPSG:25829',
-                 bbox=(-2.0, 2.0, 6.0, 5.0),
+                 srs='EPSG:4326',
+                 bbox=(-3.6934474484697,40.416876952149,-3.6840060727373,40.423002044655),#Puerta de Alcalá
                  size=(300, 250),
-                 format='image/jpeg',
+                 format='image/png',
                  transparent=True)
 
 print(img.read())
-out = open('jpl_mosaic_visb1.jpg', 'xb')
+out = open('exampleThatWorks.png', 'xb')
 y = bytes()
 y = img.read()
 out.write(y)
 out.close()
+
+#TODO a partir de una latitud y longitud obtener el encuadre para que quede en medio
+#TODO pintar una señal en el medio
+#TODO ver como muestro el recorrido de la aeronave
+#TODO Interfaz grafico
+#TODO Ver actitud, mapa y posteriormente imagen
+#TODO Marcar en un mapa waypoints para generar un path a la aeronave
